68e57c8907355cdced1139121042eff1cc634e7f
GuiValenteMM2 <guimartins.m.v@gmail.com>
2023-10-05 15:10:50 -0300

    Código refeito, iniciando lógica de click nas células

--- README.md
+++ README.md
@@ -1,23 +1,15 @@
 Jogo da velha:
-regras normais.
-Quadro do jogo: gameboard dentro de um arranjo, ativo quando escrevo os nomes dos jogadores
-e clico em jogar.
-2 jogadores, ff q define o nome e o marcador do jogador. Nome escrito no input
-Flow do jogo: Jogo começa, alterna entre os jogadores, empate se não houver combinação de 3 e 
-aparece a frase de empate, ganha se combinar 3, quando um vencer, aparecer a frase de vencedor. 
-Depois de vencer/empatar, o jogo deve ser reiniciado e as marcações apagadas.
+regras: 9 quadros, alterna os jogadores, sequencia de 3 na horizontal, vertical ou diagonal ganha.
 
-Modulo pro gameboard e flow, ff para os players.
+quadro do jogo(gameboard): Responsável pela parte visivel e interação.
+9 quadros.
+marcações dos jogadores(x, o).
 
+jogadores(players): Nome e marcador, FF.
 
-gameboard: p1 e p2, start game > gera um quadrado pra cada item de cells
-display: cells.forEach(() => {
-    const cellDiv = document.createElement('div');
-    cellDiv.classList.add('cellDiv');
-    gameBoard.appendChild(cellDiv);
-})
+jogo(game): Começar o jogo, jogadores, jogador atual, fim de jogo.
+1: renderizar os quadros, definir os jogadores.
+2: pega os nomes e passas a informação pra função de jogo.
+3: define qual será a marcação no gameboard.
+4: definir condições de vitória, restart.
 
-gameFlow: Define jogador: input X e input O; Começa jogo: botão start game;  jogador X marca,
-alterna para o jogador O; Condição de vitória e condição de empate. Botão de reiniciar. 
-
-gameFlow: inicia o game: 
--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-const gameBoard = (() => {
-    const cells = [" ", " ", " "," ", " ", " ", " ", " ", " "];
-    const gameBoardDiv = document.querySelector('#gameBoard');
-    const createBoard = cells.forEach(() => {
-        const cellButton = document.createElement('button');
-        cellButton.classList.add('cell');
-        gameBoardDiv.appendChild(cellButton);
-    });
-    
-    return {createBoard};    
+const GameBoard = (() => {
+    let cells = ["", "", "", "", "", "", "", "", ""];
+
+    const render = () => {
+        let boardHTML = "";
+        cells.forEach((cell, index) => {
+            boardHTML += `<div class="cell" id="cell-${index}">${cell}</div>`;
+        })
+        document.querySelector('#gameBoard').innerHTML = boardHTML;
+    }
+    return {render};
 })();
 
-const gameFlow = (() => {
-    const winCondition = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark};
+};
 
-    const actualPlayer = player1 ? player2 : player1;
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
 
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, "X"),
+            createPlayer(document.querySelector('#player2').value, "O")
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        GameBoard.render();
+    }
 
+    const manageClick = (cellClick) => {
+        console.log(cellClick.target.id)
+    }
+
+    return {
+        start,
+        manageClick
+    }
 })();
+
+let startButton = document.querySelector('#startButton');
+startButton.addEventListener('click', () => Game.start());
+
+let clickCell = document.querySelectorAll('.cell');
+clickCell.addEventListener('click', Game.manageClick(clickCell));
